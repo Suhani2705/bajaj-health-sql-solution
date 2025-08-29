2538230fc4780e48d64fa8f89cbb30b6dec648ff
Suhani Jain A <161123655+Suhani2705@users.noreply.github.com>
2025-08-29 10:46:14 +0530

    Create WebhookResponse.java

---
+++ WebhookResponse.java
@@ -0,0 +1,28 @@
+package com.example.bajaj.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class WebhookResponse {
+
+    @JsonProperty("webhookUrl")
+    private String webhookUrl;
+
+    @JsonProperty("accessToken")
+    private String accessToken;
+
+    public String getWebhookUrl() {
+        return webhookUrl;
+    }
+
+    public void setWebhookUrl(String webhookUrl) {
+        this.webhookUrl = webhookUrl;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
