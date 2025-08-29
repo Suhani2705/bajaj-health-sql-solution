3263787ba231efb69cdfcd8d7a7d962edafd1c7e
Suhani Jain A <161123655+Suhani2705@users.noreply.github.com>
2025-08-29 10:41:03 +0530

    Create BajajHealthSqlSolutionApplication.java

---
+++ BajajHealthSqlSolutionApplication.java
@@ -0,0 +1,12 @@
+package com.example.bajaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajHealthSqlSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BajajHealthSqlSolutionApplication.class, args);
+    }
+}
